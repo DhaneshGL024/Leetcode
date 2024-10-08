6e3809bc0422fd4f6fc499844c08d337809a73eb
DhaneshGL <dhaneshgl024@gmail.com>
2024-10-08 22:50:43 +0530

    leetcode

---
+++ 26. Remove Duplicates from Sorted Array.java
@@ -0,0 +1,16 @@
+// Remove duplicates from a sorted array in-place and return the number of unique elements
+public int removeDuplicates(int[] nums) {
+    if (nums.length == 0) {
+        return 0;
+    }
+
+    int k = 1;
+
+    for (int i = 1; i < nums.length; i++) {
+        if (nums[i] != nums[i - 1]) {
+            nums[k++] = nums[i];
+        }
+    }
+
+    return k;
+}
