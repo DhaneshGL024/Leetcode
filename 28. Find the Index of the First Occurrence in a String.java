957a8f56f2c6a9a50faa6605d8d625aeb59f7164
DhaneshGL <dhaneshgl024@gmail.com>
2024-10-09 22:37:02 +0530

    leetcode

---
+++ 28. Find the Index of the First Occurrence in a String.java
@@ -0,0 +1,4 @@
+// Problem: Find the index of the first occurrence of 'needle' in 'haystack', or -1 if 'needle' is not part of 'haystack'.
+public int strStr(String haystack, String needle) {
+    return haystack.indexOf(needle);
+}
