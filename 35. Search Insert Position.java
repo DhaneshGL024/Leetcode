badd74b0d6a168db86c19f754b72ac42c6453e68
DhaneshGL <dhaneshgl024@gmail.com>
2024-10-09 22:43:20 +0530

    leetcode

---
+++ 35. Search Insert Position.java
@@ -0,0 +1,18 @@
+// Problem: Given a sorted array of distinct integers and a target value return the index where the target is found or where it would be inserted in order. The solution must have O(log n) runtime complexity.
+public int searchInsert(int[] nums, int target) {
+    int left = 0, right = nums.length - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+}
